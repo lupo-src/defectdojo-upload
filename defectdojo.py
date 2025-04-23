60e13581d257920941a2500ca88513225b07a9f1
Wojciech Szymanski <56546969+lupo-src@users.noreply.github.com>
2025-04-23 12:49:37 +0200

    Add configurable timeout

--- defectdojo.py
+++ defectdojo.py
@@ -20,6 +20,7 @@ class Defectdojo:
         self.defectdojo_username = get_env_var("DEFECTDOJO_USERNAME")
         self.defectdojo_password = get_env_var("DEFECTDOJO_PASSWORD")
         self.defectdojo_iap_token = get_env_var("DEFECTDOJO_IAP_TOKEN")
+        self.defectdojo_timeout = get_env_var("DEFECTDOJO_UPLOAD_TIMEOUT")
 
         # Required Defectdojo Variables.
         self.defectdojo_url = get_env_var("DEFECTDOJO_URL")
@@ -27,8 +28,6 @@ class Defectdojo:
         self.defectdojo_product = get_env_var("DEFECTDOJO_PRODUCT")
         self.defectdojo_environment_type = get_env_var("DEFECTDOJO_ENVIRONMENT_TYPE")
         self.defectdojo_scan_type = get_env_var("DEFECTDOJO_SCAN_TYPE")
-        self.defectdojo_engagement_name = get_env_var("DEFECTDOJO_ENGAGEMENT_NAME")
-        self.defectdojo_engagement_name = get_env_var("DEFECTDOJO_ENGAGEMENT_NAME")
         self.defectdojo_engagement_name = get_env_var("DEFECTDOJO_ENGAGEMENT_NAME")
         self.scan_results_file_path = get_env_var("RESULTS_FILE_PATH")
 
@@ -68,7 +67,7 @@ class Defectdojo:
                         data=data,
                         verify=True,
                         cert=(client_certificate_file_path, client_key_file_path),
-                        timeout=180
+                        timeout=self.defectdojo_timeout 
                     )
                 else:
                     r = requests.post(
@@ -77,7 +76,7 @@ class Defectdojo:
                         files=files,
                         data=data,
                         verify=True,
-                        timeout=180
+                        timeout=self.defectdojo_timeout 
                     )
 
                 if r.status_code == 201:
